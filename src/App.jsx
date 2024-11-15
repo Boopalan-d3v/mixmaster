d488bae4ffbd2a2704c62743555dab868e024416
Boopalan-d3v <breacher.darkworld@gmail.com>
2024-11-15 09:43:52 +0530

    add router on App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <h2>home page</h2>,
+  },
+  {
+    path:'/about',
+    element: <h2>about page</h2>,
+  },
+])
+
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+  return <RouterProvider router = {router} />;
 };
 export default App;
