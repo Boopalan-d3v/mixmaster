9f969fa49c7fadf35fb761b9843163048e18146a
Boopalan-d3v <breacher.darkworld@gmail.com>
2024-11-27 16:09:32 +0530

    add styled-component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledBtn = styled.button`
+  background: red;
+  color: white;
+  font-size:2rem;
+  padding: 1rem;
+`;
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav >
+        <div className='nav-center'>
+            <span className='logo'>MixMaster</span>
+            <div className='nav-links'>
+                <NavLink to='/' className='nav-link'>Home</NavLink>
+                <NavLink to='/about' className='nav-link'>About</NavLink>
+                <NavLink to='/newsletter' className='nav-link'>Newsletter</NavLink>
+            </div>
+        </div>
+        <StyledBtn>hello</StyledBtn>
+    </nav>
   )
 }
 
