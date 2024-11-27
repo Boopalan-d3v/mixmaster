8c5f2578fc83c22a5e94a8034fff1044f5c045e3
Boopalan-d3v <breacher.darkworld@gmail.com>
2024-11-27 16:09:03 +0530

    import navbar component

--- src/Pages/HomeLayout.jsx
+++ src/Pages/HomeLayout.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
+import Navbar from '../Components/Navbar'
 
 
 
 const HomeLayout = () => {
   return (
     <div>
-      <nav>navbar</nav>
+      <Navbar/>
       <Outlet/>
     </div>
   )
