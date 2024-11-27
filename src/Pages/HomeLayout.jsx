b392e4278d5cf7d7b9d9d454fafa715e84db53cf
Boopalan-d3v <breacher.darkworld@gmail.com>
2024-11-27 14:39:45 +0530

    add Outlet obj from react-router-dom

--- src/Pages/HomeLayout.jsx
+++ src/Pages/HomeLayout.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {Outlet} from 'react-router-dom'
+
+
 
 const HomeLayout = () => {
   return (
-    <h1>HomeLayout</h1>
+    <div>
+      <nav>navbar</nav>
+      <Outlet/>
+    </div>
   )
 }
 
